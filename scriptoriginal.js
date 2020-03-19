9b0f59608c9bbc1f0375a7f8e0006b8b9f93919a
Michael Cassaro <mdcassaro@hotmail.com>
2020-03-19 18:42:43 -0500

    put a max and min on it

--- scriptoriginal.js
+++ scriptoriginal.js
@@ -7,12 +7,126 @@ var userPreference1 = confirm("Do you want to include Letters?");
 var userPreference2 = confirm("Do you want to include Numbers?");
 var userPreference3 = confirm("Do you want to include Symbols?");
 var stringLengthUser = prompt("How long do you want your password?")
-var letterAndNumbers = (userPreference1 && userPreference2)
-var lettersAndSymbols = (userPreference1 && userPreference3)
-var numbersAndSymbols = (userPreference2 && userPreference3)
+
+var userletters = (userPreference1)
+var usernumbers = (userPreference2)
+var userSymbols = (userPreference3)
 
 
+if(stringLengthUser < 128 && stringLengthUser >8){
+    function generatePassword(){
+        var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz!@#$%^&*(){}[]?";
+        var string_length = parseInt(stringLengthUser);
+        var randomstring = "";
+        for (var i=0; i<string_length; i++) {
+            var rnum = Math.floor(Math.random() * chars.length);
+            randomstring += chars.substring(rnum,rnum+1);
+        }
+        return randomstring
+        console.log(main)
+        
+        
+    }
+    function generateNumbsAndLetters(){
+        var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxy";
+        var string_length = parseInt(stringLengthUser);
+        var randomstring = "";
+        for (var i=0; i<string_length; i++) {
+            var rnum = Math.floor(Math.random() * chars.length);
+            randomstring += chars.substring(rnum,rnum+1);
+        }
+        return randomstring    
+        console.log(2)
+    }
+    function generateNumbsAndSymbols(){
+            var chars = "0123456789!@#$%^&*(){}[]?";
+            var string_length = parseInt(stringLengthUser);
+            var randomstring = "";
+            for (var i=0; i<string_length; i++) {
+                var rnum = Math.floor(Math.random() * chars.length);
+                randomstring += chars.substring(rnum,rnum+1);
+            return randomstring
+            console.log(3)
+            }
+    }
+    function generateLettersAndSymbols(){
+        var chars = "!@#$%^&*(){}[]?ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+        var string_length = parseInt(stringLengthUser);
+        var randomstring = "";
+        for (var i=0; i<string_length; i++) {
+            var rnum = Math.floor(Math.random() * chars.length);
+            randomstring += chars.substring(rnum,rnum+1);
+        }
+        
+        return randomstring
+        console.log(4)
+        
+    }
+    function generateLetters(){
+        var chars = "ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+        var string_length = parseInt(stringLengthUser);
+        var randomstring = "";
+        for (var i=0; i<string_length; i++) {
+            var rnum = Math.floor(Math.random() * chars.length);
+            randomstring += chars.substring(rnum,rnum+1);
+        }
+        
+        return randomstring
+        console.log(5)
+        
+    }
+    function generateSymbols(){
+        var chars = "!@#$%^&*(){}[]?";
+        var string_length = parseInt(stringLengthUser);
+        var randomstring = "";
+        for (var i=0; i<string_length; i++) {
+            var rnum = Math.floor(Math.random() * chars.length);
+            randomstring += chars.substring(rnum,rnum+1);
+        }
+        
+        
+        return randomstring
+        console.log(6)
+        
+    }
+    function generateNumbers(){
+        var chars = "0123456789";
+        var string_length = parseInt(stringLengthUser);
+        var randomstring = "";
+        for (var i=0; i<string_length; i++) {
+            var rnum = Math.floor(Math.random() * chars.length);
+            randomstring += chars.substring(rnum,rnum+1);
+        }
+        
+        return randomstring
+        
+        
+    }
+    if (userletters == true && userSymbols == true && usernumbers == true){
+        generatePassword()
+    
+    }else if(userletters === true && userSymbols == false && usernumbers == false){
+            generateLetters()
+    
+    }else if (usernumbers === true && userletters === false && userSymbols == false){
+            generateNumbers()
+    }else if(userSymbols === true && usernumbers === false && userletters === false){
+            generateSymbols()
+    }else if(userletters === true && usernumbers === true && userSymbols === false){
+            generateNumbsAndLetters()
+    }else if(userletters === true && userSymbols===true && usernumbers ===false){
+            generateLettersAndSymbols()
+    }else if(usernumbers === true && userSymbols === true && userletters === false){
+            generateNumbsAndSymbols()
+    }  
+   
+    
 
+
+}else{
+    alert("Put in a Length between 8-128")
+    var stringLengthUser = prompt("How long do you want your password?")
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -25,108 +139,6 @@ function writePassword() {
 
 }
 //write generate password function my self.
-function generatePassword(){
-    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz!@#$%^&*(){}[]?";
-    var string_length = parseInt(stringLengthUser);
-	var randomstring = '';
-	for (var i=0; i<string_length; i++) {
-		var rnum = Math.floor(Math.random() * chars.length);
-		randomstring += chars.substring(rnum,rnum+1);
-    }
-    return randomstring
-    console.log(main)
-    
-    
-}
-function generateNumbsAndLetters(){
-    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxy";
-    var string_length = parseInt(stringLengthUser);
-	var randomstring = '';
-	for (var i=0; i<string_length; i++) {
-		var rnum = Math.floor(Math.random() * chars.length);
-        randomstring += chars.substring(rnum,rnum+1);
-    }
-    return randomstring    
-    console.log(2)
-}
-function generateNumbsAndSymbols(){
-        var chars = "0123456789!@#$%^&*(){}[]?";
-        var string_length = parseInt(stringLengthUser);
-        var randomstring = '';
-        for (var i=0; i<string_length; i++) {
-            var rnum = Math.floor(Math.random() * chars.length);
-            randomstring += chars.substring(rnum,rnum+1);
-        return randomstring
-        console.log(3)
-        }
-}
-function generateLettersAndSymbols(){
-    var chars = "!@#$%^&*(){}[]?ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
-    var string_length = parseInt(stringLengthUser);
-    var randomstring = '';
-    for (var i=0; i<string_length; i++) {
-        var rnum = Math.floor(Math.random() * chars.length);
-        randomstring += chars.substring(rnum,rnum+1);
-    }
-    
-    return randomstring
-    console.log(4)
-    
-}
-function generateLetters(){
-    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
-    var string_length = parseInt(stringLengthUser);
-    var randomstring = '';
-    for (var i=0; i<string_length; i++) {
-        var rnum = Math.floor(Math.random() * chars.length);
-        randomstring += chars.substring(rnum,rnum+1);
-    }
-    
-    return randomstring
-    console.log(5)
-    
-}
-function generateSymbols(){
-    var chars = "!@#$%^&*(){}[]?";
-    var string_length = parseInt(stringLengthUser);
-    var randomstring = '';
-    for (var i=0; i<string_length; i++) {
-        var rnum = Math.floor(Math.random() * chars.length);
-        randomstring += chars.substring(rnum,rnum+1);
-    }
-    
-    return randomstring
-    console.log(6)
-    
-}
-function generateNumbers(){
-    var chars = "0123456789";
-    var string_length = parseInt(stringLengthUser);
-    var randomstring = '';
-    for (var i=0; i<string_length; i++) {
-        var rnum = Math.floor(Math.random() * chars.length);
-        randomstring += chars.substring(rnum,rnum+1);
-    }
-    
-    return randomstring
-    console.log(7)
-    
-}
-
-if(userPreference1 === true){
-    generateLetters()
-
-}else if (userPreference2 === true){
-    generateNumbers()
-}else if(userPreference3 === true){
-    generateSymbols()
-}else if(letterAndNumbers === true){
-    generateNumbsAndLetters()
-}else if(lettersAndSymbols === true){
-    generateLettersAndSymbols()
-}else if(numbersAndSymbols === true){
-    generateNumbsAndSymbols()
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
---
+++ web.js
@@ -0,0 +1,11 @@
+function random_password_generate(max,min)
+{
+    var passwordChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#@!%&()/";
+    var randPwLen = Math.floor(Math.random() * (max - min + 1)) + min;
+    var randPassword = Array(randPwLen).fill(passwordChars).map(function(x) { return x[Math.floor(Math.random() * x.length)] }).join('');
+    return randPassword;
+}
+document.getElementById("generate").addEventListener("click", function(){
+    random_password = random_password_generate(16,8);
+    document.getElementById("randomPassword").value = random_password;
+});
